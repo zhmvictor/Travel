98aad0079c94f467af7f22d3005f0573575e2e3a
zhouhuimin <zhouhuimin@boco.com.cn>
2019-07-25 20:39:37 +0800

    首页视图开发完整版

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,23 +1,17 @@
 <template>
-  <div>
-    <div class="title">热销推荐</div>
-    <ul>
-      <router-link
-        tag="li"
-        class="item border-bottom"
-        v-for="item of list"
-        :key="item.id"
-        :to="'/detail/' + item.id"
-      >
-        <img class="item-img" :src="item.imgUrl" />
-        <div class="item-info">
-          <p class="item-title">{{item.title}}</p>
-          <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
-        </div>
-      </router-link>
-    </ul>
-  </div>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item" v-for="list of list" :key="list.id">
+                <img :src=list.imgUrl class="item-img">
+                <div class="item-info">
+                    <p class="item-title">{{list.title}}</p>
+                    <p class="item-desc">{{list.desc}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
@@ -25,42 +19,47 @@ export default {
   name: 'HomeRecommend',
   props: {
     list: Array
+  },
+  data () {
+    return {
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/mixins.styl'
-  .title
-    margin-top: .2rem
-    line-height: .8rem
-    background: #eee
-    text-indent: .2rem
-  .item
-    overflow: hidden
-    display: flex
-    height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.7rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      padding: .1rem
-      min-width: 0
-      .item-title
-        line-height: .54rem
-        font-size: .32rem
-        ellipsis()
-      .item-desc
-        line-height: .4rem
-        color: #ccc
-        ellipsis()
-      .item-button
-        line-height: .44rem
-        margin-top: .16rem
-        background: #ff9300
-        padding: 0 .2rem
-        border-radius: .06rem
-        color: #fff
+    @import '~styles/mixins.styl'
+    .title
+        margin-top: .2rem
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+    .item
+        display: flex
+        height: 1.9rem
+        overflow: hidden
+        .item-img
+            width: 1.7rem
+            height: 1.7rem
+            padding: .1rem
+        .item-info
+            flex: 1
+            padding: .1rem
+            min-width: 0
+            .item-title
+                line-height: .54rem
+                font-size: .32rem
+                ellipsis()
+            .item-desc
+                line-height: .4rem
+                color: #ccc
+                ellipsis()
+            .item-button
+                line-height: .44rem
+                margin-top: .16rem
+                background: #ff9300
+                padding: 0 .2rem
+                border-radius: .06rem
+                color: #fff
+
 </style>
